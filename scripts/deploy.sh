cc7fdb71155d4745655c167650e98b2cc44ddc05
stan1ee0 <stan1ee@naver.com>
2023-06-01 16:25:24 +0900

    Update github actions

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # 빌드 파일의 이름이 콘텐츠와 다르다면 다음 줄의 .jar 파일 이름을 수정하시기 바랍니다.
-BUILD_JAR=$(ls /home/ubuntu/action/build/libs/simpletodos-0.0.1-SNAPSHOT.jar)
+APP_NAME=simpletodos-1.0
+BUILD_JAR=$(ls /home/ubuntu/action/build/libs/$APP_NAME.jar)
 JAR_NAME=$(basename $BUILD_JAR)
 
 echo "> 현재 시간: $(date)" >> /home/ubuntu/action/deploy.log
@@ -22,7 +23,6 @@ else
   sudo kill -9 $CURRENT_PID
   sleep 5
 fi
-
 
 DEPLOY_JAR=$DEPLOY_PATH$JAR_NAME
 echo "> DEPLOY_JAR 배포"    >> /home/ubuntu/action/deploy.log
