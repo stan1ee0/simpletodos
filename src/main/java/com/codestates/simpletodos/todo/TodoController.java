49f98c4d4f077531d48d4323f93510f1da03bc5e
stan1ee0 <stan1ee@naver.com>
2023-06-05 15:18:41 +0900

    Changed to run as WAS application and to use requestUrl from HttpServletRequest

--- src/main/java/com/codestates/simpletodos/todo/TodoController.java
+++ src/main/java/com/codestates/simpletodos/todo/TodoController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -11,8 +12,6 @@ import java.util.stream.Collectors;
 @CrossOrigin
 @RestController
 public class TodoController {
-    private final String baseUrl = "http://localhost:8080/";
-
     private TodoService todoService;
 
     @Autowired
@@ -22,34 +21,41 @@ public class TodoController {
 
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping
-    public TodoDto postTodo(@Valid @RequestBody TodoDto todoDto) {
+    public TodoDto postTodo(@Valid @RequestBody TodoDto todoDto,
+                            HttpServletRequest request) {
         Todo createdTodo = todoService.createTodo(new Todo(todoDto));
 
-        return new TodoDto(createdTodo, baseUrl + String.valueOf(createdTodo.getId()));
+        String requestUrl = request.getRequestURL().toString();
+        return new TodoDto(createdTodo, requestUrl + String.valueOf(createdTodo.getId()));
     }
 
     @GetMapping
-    public List<TodoDto> getAllTodos() {
+    public List<TodoDto> getAllTodos(HttpServletRequest request) {
         List<Todo> allTodos = todoService.findAllTodos();
 
+        String requestUrl = request.getRequestURL().toString();
         return allTodos.stream()
-                .map(todo -> new TodoDto(todo, baseUrl + String.valueOf(todo.getId())))
+                .map(todo -> new TodoDto(todo, requestUrl + String.valueOf(todo.getId())))
                 .collect(Collectors.toList());
     }
 
     @GetMapping(value = "/{id}")
-    public TodoDto getTodo(@PathVariable long id) {
+    public TodoDto getTodo(@PathVariable long id,
+                           HttpServletRequest request) {
         Todo foundTodo = todoService.findTodo(id);
 
-        return new TodoDto(foundTodo, baseUrl + String.valueOf(foundTodo.getId()));
+        String requestUrl = request.getRequestURL().toString();
+        return new TodoDto(foundTodo, requestUrl);
     }
 
     @PatchMapping(value = "/{id}")
     public TodoDto patchTodo(@PathVariable long id,
-                          @RequestBody TodoDto todoDto) {
+                             @RequestBody TodoDto todoDto,
+                             HttpServletRequest request) {
         Todo updatedTodo = todoService.updateTodo(new Todo(todoDto), id);
 
-        return new TodoDto(updatedTodo, baseUrl + String.valueOf(updatedTodo.getId()));
+        String requestUrl = request.getRequestURL().toString();
+        return new TodoDto(updatedTodo, requestUrl);
     }
 
     @ResponseStatus(HttpStatus.NO_CONTENT)
